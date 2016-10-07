b4e4b85bddff987aed942233c28003303de53cb1
Jason Jones <jason@gaincompliance.com>
2016-10-07 14:49:17 -0500

    first attempt at pip installable

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = 'ndb-audit',
+    packages = ['ndb-audit'],
+    version = '0.0.1',
+    description = 'Adds audit trail to any NDB entity',
+    author='Jason Jones',
+    author_email='jason@gaincompliance.com',
+    url='https://github.com/GainCompliance/ndb-audit',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2'
+    ]
+)
