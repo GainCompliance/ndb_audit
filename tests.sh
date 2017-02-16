81490767d31f8d822f1d4dc316fa61b691d1c7f4
Jason Jones <jason@gaincompliance.com>
2017-02-16 08:36:14 -0600

    test(nose-fix): remove exclude-dirs

--- tests.sh
+++ tests.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nosetests . --with-coverage --cover-package=ndb_audit --cover-html --cover-html-dir=./cover-html --with-gae --gae-lib-root=$GAE_LIB_ROOT --processes=-1 --exclude-dir-file=./exclude_dirs.txt
+nosetests . --with-coverage --cover-package=ndb_audit --cover-html --cover-html-dir=./cover-html --with-gae --gae-lib-root=$GAE_LIB_ROOT --processes=-1
